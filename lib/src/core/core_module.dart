56717a4caadb7993ff95280ffadb45480e68841a
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2026-01-14 16:09:39 -0800

    u1

--- lib/src/core/core_module.dart
+++ lib/src/core/core_module.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: directives_ordering
+
+
 //! --> Exporting modules
 export 'constants/core_view_messages.dart';
+export 'utils/validation_utils.dart';
 
 //! --> Exporting [Formatters]
 export 'formatters/email_formatter.dart';
