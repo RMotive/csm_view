1e5f4583a54efd5a6fb1408b341db3434317325c
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2026-01-14 17:08:30 -0800

    t4

--- lib/src/abstractions/bases/view_whisper_form_base.dart
+++ lib/src/abstractions/bases/view_whisper_form_base.dart
@@ -35,9 +35,6 @@ abstract class ViewWhisperFormBase extends ViewPageBase implements IViewWhisperF
   @override
   @Deprecated('Dont override this method, use composeForm instead')
   Widget compose(BuildContext context, Size windowSize, Size pageSize) {
-    /// [Form] state proxy.
-    final GlobalKey<FormState> formStateKey = GlobalKey<FormState>();
-
     /// Theming data.
     IThemeData themeData = ThemingUtils.get(context);
 
@@ -66,79 +63,83 @@ abstract class ViewWhisperFormBase extends ViewPageBase implements IViewWhisperF
               ),
               child: ColoredBox(
                 color: themeData.page.back,
-                child: Column(
-                  children: <Widget>[
-                    // --> Whisper Header
-                    ColoredBox(
-                      color: themeData.page.accent,
-                      child: SizedBox(
-                        width: double.maxFinite,
-                        child: Padding(
-                          padding: EdgeInsetsGeometry.symmetric(
-                            vertical: 12,
-                            horizontal: 18,
-                          ),
-                          child: Text(
-                            title,
-                            textAlign: TextAlign.start,
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: themeData.page.foreAlt,
-                              fontWeight: FontWeight.w600,
-                              fontStyle: FontStyle.italic,
+                child: _FormStateHandler(
+                  builder: (BuildContext context, GlobalKey<FormState> stateKey) {
+                    return Column(
+                      children: <Widget>[
+                        // --> Whisper Header
+                        ColoredBox(
+                          color: themeData.page.accent,
+                          child: SizedBox(
+                            width: double.maxFinite,
+                            child: Padding(
+                              padding: EdgeInsetsGeometry.symmetric(
+                                vertical: 12,
+                                horizontal: 18,
+                              ),
+                              child: Text(
+                                title,
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: themeData.page.foreAlt,
+                                  fontWeight: FontWeight.w600,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ),
 
-                    // --> Whisper Content
-                    Expanded(
-                      child: Form(
-                        key: formStateKey,
-                        child: composeForm(formStateKey, context, windowSize, pageSize),
-                      ),
-                    ),
-
-                    // --> Whisper Footer
-                    SizedBox(
-                      width: double.maxFinite,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 12,
-                          horizontal: 18,
+                        // --> Whisper Content
+                        Expanded(
+                          child: Form(
+                            key: stateKey,
+                            child: composeForm(stateKey, context, windowSize, pageSize),
+                          ),
                         ),
-                        child: Row(
-                          spacing: 16,
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            //* --> Close Action Button
-                            ButtonFlat(
-                              label: 'Close',
-                              width: controlsWidth,
-                              theming: themeData.controlError,
-                              onClick: () {
-                                Navigator.of(context).pop();
 
-                                onClose();
-                              },
+                        // --> Whisper Footer
+                        SizedBox(
+                          width: double.maxFinite,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 12,
+                              horizontal: 18,
                             ),
+                            child: Row(
+                              spacing: 16,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                //* --> Close Action Button
+                                ButtonFlat(
+                                  label: 'Close',
+                                  width: controlsWidth,
+                                  theming: themeData.controlError,
+                                  onClick: () {
+                                    Navigator.of(context).pop();
 
-                            //* --> Perform Action Button
-                            ButtonFlat(
-                              label: 'Perform',
-                              width: controlsWidth,
-                              onClick: () {
-                                if (formStateKey.currentState!.validate()) {
-                                  onPerform();
-                                }
-                              },
+                                    onClose();
+                                  },
+                                ),
+
+                                //* --> Perform Action Button
+                                ButtonFlat(
+                                  label: 'Perform',
+                                  width: controlsWidth,
+                                  onClick: () {
+                                    if (stateKey.currentState!.validate()) {
+                                      onPerform();
+                                    }
+                                  },
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                  ],
+                      ],
+                    );
+                  },
                 ),
               ),
             ),
@@ -146,5 +147,30 @@ abstract class ViewWhisperFormBase extends ViewPageBase implements IViewWhisperF
         );
       },
     );
+  }
+}
+
+/// Draws a [StatefulWidget] that handles a preserved [FormState] key.
+final class _FormStateHandler extends StatefulWidget {
+  /// Child buider.
+  final Widget Function(BuildContext context, GlobalKey<FormState> stateKey) builder;
+
+  /// Builds a new instance.
+  const _FormStateHandler({
+    required this.builder,
+  });
+
+  @override
+  State<_FormStateHandler> createState() => __FormStateHandlerState();
+}
+
+/// [State] class [_FormStateHandler].
+final class __FormStateHandlerState extends State<_FormStateHandler> {
+  /// Inner [Form] state key.
+  final GlobalKey<FormState> _formState = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(context, _formState);
   }
 }
