5c96334d2cf73f7074d3cb0bc1a143edc431b084
Renato Urrea <usuaryrenato@hotmail.com>
2026-08-11 15:24:26 -0700

    changing to a wrap usage

--- lib/src/view/sandbox/_page_sandbox_welcome/_package_sandbox_welcome.dart
+++ lib/src/view/sandbox/_page_sandbox_welcome/_package_sandbox_welcome.dart
@@ -65,13 +65,44 @@ final class _PackageSandboxWelcome<ThemeBase extends PackageSandboxThemeBase> ex
             ),
           ),
 
-          // Dashboard entries access section.
+          //* Sandbox entries cards.
           Expanded(
             child: Padding(
               padding: EdgeInsets.all(16),
-              child: ColoredSizedBox(
-                background: Colors.orange,
-                size: Size(100, 100),
+              child: SizedBox.expand(
+                child: LayoutBuilder(
+                  builder: (BuildContext viewContext, BoxConstraints cardsContainerConstraints) {
+                    const BoxConstraints cardConstraints = BoxConstraints(
+                      maxWidth: 425,
+                      minWidth: 225,
+                    );
+                    cardsContainerConstraints = cardsContainerConstraints.normalize();
+                    double widthSpace = cardsContainerConstraints.biggest.width;
+                    double cardWidth = widthSpace / 4;
+
+                    return SingleChildScrollView(
+                      child: Wrap(
+                        children: routingGraph.entries.map<Widget>(
+                          (MapEntry<RouteData, IPackageSandboxEntry<ThemeBase>> sandboxRoutedEntry) {
+                            return ConstrainedBox(
+                              constraints: cardConstraints,
+                              child: AspectRatio(
+                                aspectRatio: 2 / 1,
+                                child: SizedBox(
+                                  width: cardWidth,
+                                  child: _PackageSandboxWelcomeItemCard<ThemeBase>(
+                                    routeData: sandboxRoutedEntry.key,
+                                    sandboxItem: sandboxRoutedEntry.value,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ).toList(),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           )
